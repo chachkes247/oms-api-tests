8edccf5a1dbe6fe9335bd282432fca0b1bc30a97
qzlbn9_gme <avishay.chachkes@gm.com>
2025-04-14 17:16:50 +0300

    adding docker-compose down

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -1,15 +1,9 @@
 from pymongo import MongoClient
 
 
-def test_example():
-    print("\n\n\n1\n\n\n")
-    assert 1 == 1  # Simple sanity test to ensure pytest is working
 
-
-    
 
 def test_mongo_insert():
-    print("\n\n\n2\n\n\n")
     client = MongoClient("mongodb://localhost:27017/")
     db = client.test_db
     collection = db.test_collection
@@ -17,7 +11,6 @@ def test_mongo_insert():
     assert result.inserted_id is not None
 
 def test_mongo_query():
-    print("\n\n\n3\n\n\n")
     client = MongoClient("mongodb://localhost:27017/")
     db = client.test_db
     collection = db.test_collection
