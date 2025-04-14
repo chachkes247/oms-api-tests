275ab1140273b4d88ef916eeff0e3be86a16247c
qzlbn9_gme <avishay.chachkes@gm.com>
2025-04-14 17:09:23 +0300

    adding a test to validate mongo

--- tests/test_orders.py
+++ tests/test_orders.py
@@ -2,12 +2,14 @@ from pymongo import MongoClient
 
 
 def test_example():
+    print("\n\n\n1\n\n\n")
     assert 1 == 1  # Simple sanity test to ensure pytest is working
 
 
     
 
 def test_mongo_insert():
+    print("\n\n\n2\n\n\n")
     client = MongoClient("mongodb://localhost:27017/")
     db = client.test_db
     collection = db.test_collection
@@ -15,6 +17,7 @@ def test_mongo_insert():
     assert result.inserted_id is not None
 
 def test_mongo_query():
+    print("\n\n\n3\n\n\n")
     client = MongoClient("mongodb://localhost:27017/")
     db = client.test_db
     collection = db.test_collection
